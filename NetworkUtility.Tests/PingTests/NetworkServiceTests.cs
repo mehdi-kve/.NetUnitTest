d9758e0e560a07f6baf181b181ba4c7e9edb981a
m3hdi <mehdi.k1917@gmail.com>
2024-09-25 15:39:33 +0330

    Initial Project

---
+++ NetworkUtility.Tests/PingTests/NetworkServiceTests.cs
@@ -0,0 +1,29 @@
+﻿using FluentAssertions;
+using NetworkUtility.Ping;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace NetworkUtility.Tests.PingTest
+{
+    public class NetworkServiceTests
+    {
+        [Fact]
+        public void NetworkService_SendPing_ReturnString() 
+        {
+            // Arrange - variables, classes , mocks
+            var pingService = new NetworkService();
+
+            // Act 
+            var result = pingService.SendPing();
+
+            // Assert As Execution part --using fluent assertion
+            result.Should().NotBeNullOrWhiteSpace();
+            result.Should().Be("Success: Ping Sent!");
+            result.Should().Contain("Success", Exactly.Once());
+        }
+    }
+}
